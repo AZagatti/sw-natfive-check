f31c22fa3c376742952179a56f8dd5732b64c499
azagatti <andre.zagatti1@gmail.com>
2020-08-21 16:15:32 -0300

    fix: unexpected token

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -54,8 +54,6 @@ export default class MyDocument extends Document<DocumentProps> {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                <!-- Global site tag (gtag.js) - Google Analytics -->
-                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-176080246-1"></script>
                 <script>
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
